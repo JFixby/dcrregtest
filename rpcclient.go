2311f6bfb3dfe2eeefd22c7b0a10c7f70a759c69
J Fixby <github@jfixby.com>
2019-06-04 19:39:47 +0200

    add rpcclient

---
+++ rpcclient.go
@@ -0,0 +1,18 @@
+// Copyright (c) 2018 The btcsuite developers
+// Copyright (c) 2018 The Decred developers
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package dcrregtest
+
+import (
+	"github.com/decred/dcrd/rpcclient"
+	"github.com/jfixby/cointest"
+)
+
+type DcrRPCClientFactory struct {
+}
+
+func (f *DcrRPCClientFactory) NewRPCConnection(config cointest.RPCConnectionConfig, handlers cointest.RPCClientNotificationHandlers) (cointest.RPCClient, error) {
+	return rpcclient.New(config.(*rpcclient.ConnConfig), handlers.(*rpcclient.NotificationHandlers))
+}
