c3dc0ab03fc0e5ed5983f561926cd323b8916ab5
J Fixby <github@jfixby.com>
2019-09-20 19:49:22 +0200

    update release

--- go.mod
+++ go.mod
@@ -4,15 +4,19 @@ go 1.12
 
 require (
 	github.com/decred/dcrd/blockchain v1.2.0 // indirect
+	github.com/decred/dcrd/blockchain/stake v1.2.1
 	github.com/decred/dcrd/chaincfg v1.5.2
 	github.com/decred/dcrd/chaincfg/chainhash v1.0.2
 	github.com/decred/dcrd/dcrjson v1.2.0
 	github.com/decred/dcrd/dcrutil v1.4.0
-	github.com/decred/dcrd/hdkeychain v1.1.1 // indirect
 	github.com/decred/dcrd/rpcclient v1.1.0
+	github.com/decred/dcrd/txscript v1.1.0
 	github.com/decred/dcrd/wire v1.2.0
 	github.com/decred/dcrwallet/errors v1.1.0
-	github.com/jfixby/coinharness v0.0.0-20190912100401-a5152c2a8eae
-	github.com/jfixby/dcrharness v0.0.0-20190912100457-320d0d5aeb20
+	github.com/decred/dcrwallet/wallet v1.3.0
+	github.com/google/go-cmp v0.3.1
+	github.com/jfixby/coinharness v0.0.0-20190920172906-6a043d114519
+	github.com/jfixby/dcrharness v0.0.0-20190920174811-34ce2af61273
 	github.com/jfixby/pin v0.0.0-20190810181909-8c18ae7f9c3c
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 )
--- run_tests.sh
+++ run_tests.sh
@@ -1,32 +1,6 @@
 #!/usr/bin/env bash
 
-# usage:
-# ./run_tests.sh                         # local, go 1.12
-# GOVERSION=1.11 ./run_tests.sh          # local, go 1.11
-# ./run_tests.sh docker                  # docker, go 1.12
-# GOVERSION=1.11 ./run_tests.sh docker   # docker, go 1.11
-# ./run_tests.sh podman                  # podman, go 1.12
-# GOVERSION=1.11 ./run_tests.sh podman   # podman, go 1.11
-
 set -ex
-
-# The script does automatic checking on a Go package and its sub-packages,
-# including:
-# 1. gofmt         (https://golang.org/cmd/gofmt/)
-# 2. gosimple      (https://github.com/dominikh/go-simple)
-# 3. unconvert     (https://github.com/mdempsky/unconvert)
-# 4. ineffassign   (https://github.com/gordonklaus/ineffassign)
-# 5. go vet        (https://golang.org/cmd/vet)
-# 6. misspell      (https://github.com/client9/misspell)
-# 7. race detector (https://blog.golang.org/race-detector)
-
-# golangci-lint (github.com/golangci/golangci-lint) is used to run each each
-# static checker.
-
-# To run on docker on windows, symlink /mnt/c to /c and then execute the script
-# from the repo path under /c.  See:
-# https://github.com/Microsoft/BashOnWindows/issues/1854
-# for more details.
 
 # Default GOVERSION
 [[ ! "$GOVERSION" ]] && GOVERSION=1.12
@@ -34,10 +8,11 @@ REPO=dcrregtest
 
 testrepo () {
   GO=go
+  PROJECT=decred
+  NODE_REPO=dcrd
+  WALLET_REPO=dcrwallet
 
   $GO version
-  #dcrd --version
-  #dcrwallet --version
 
   # binary needed for RPC tests
   env CC=gcc
@@ -45,21 +20,28 @@ testrepo () {
   # run tests on all modules
 
   pushd ../../
-  git clone --depth=50 --branch=release-v1.4 https://github.com/decred/dcrd.git decred/dcrd
-  git clone --depth=50 --branch=release-v1.4 https://github.com/decred/dcrwallet.git decred/dcrwallet
+  git clone --depth=50 --branch=release-v1.4 https://github.com/${PROJECT}/${NODE_REPO}.git ${PROJECT}/${NODE_REPO}
+  git clone --depth=50 --branch=release-v1.4 https://github.com/${PROJECT}/${WALLET_REPO}.git ${PROJECT}/${WALLET_REPO}
   popd
 
   $GO fmt ./...
   $GO build ./...
 
-  pushd ../../decred/dcrd
+  pushd ../../${PROJECT}/${NODE_REPO}
+  $GO build ./...
+  ${NODE_REPO} --version
   $GO install
   popd
 
-  pushd ../../decred/dcrwallet
+  pushd ../../${PROJECT}/${WALLET_REPO}
+  $GO build ./...
+  ${WALLET_REPO} --version
   $GO install
   popd
 
+  GO111MODULE=on
+  $GO clean -testcache
+  $GO build ./...
   $GO test ./...
 
   echo "------------------------------------------"
