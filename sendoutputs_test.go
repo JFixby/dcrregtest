60d9d239c8f3ab52b21dde60e107cb2bc58f7198
J Fixby <github@jfixby.com>
2019-09-22 21:33:32 +0200

    refactor send ops

--- sendoutputs_test.go
+++ sendoutputs_test.go
@@ -1,69 +1,12 @@
 package dcrregtest
 
 import (
-	"github.com/decred/dcrd/rpcclient"
+	"github.com/decred/dcrd/wire"
 	"github.com/jfixby/coinharness"
 	"github.com/jfixby/dcrharness"
 	"github.com/jfixby/pin"
 	"testing"
-
-	"github.com/decred/dcrd/chaincfg/chainhash"
-	"github.com/decred/dcrd/dcrutil"
-	"github.com/decred/dcrd/txscript"
-	"github.com/decred/dcrd/wire"
 )
-
-func genSpend(t *testing.T, r *coinharness.Harness, amt coinharness.CoinsAmount) coinharness.Hash {
-	// Grab a fresh address from the wallet.
-	addr, err := r.Wallet.NewAddress(&coinharness.NewAddressArgs{"default"})
-	if err != nil {
-		t.Fatalf("unable to get new address: %v", err)
-	}
-
-	// Next, send amt to this address, spending from one of our
-	// mature coinbase outputs.
-	addrScript, err := txscript.PayToAddrScript(addr.Internal().(dcrutil.Address))
-	if err != nil {
-		t.Fatalf("unable to generate pkscript to addr: %v", err)
-	}
-
-	output := &coinharness.TxOut{
-		Amount:   amt,
-		PkScript: addrScript,
-		Version:  wire.DefaultPkScriptVersion,
-	}
-	arg := &coinharness.CreateTransactionArgs{
-		Outputs:         []*coinharness.TxOut{output},
-		FeeRate:         coinharness.CoinsAmountFromFloat(10),
-		PayToAddrScript: dcrharness.PayToAddrScript,
-		TxSerializeSize: dcrharness.TxSerializeSize,
-	}
-
-	txid, err := coinharness.CreateTransaction(r.Wallet, arg)
-	if err != nil {
-		t.Fatalf("coinbase spend failed: %v", err)
-	}
-	return txid.TxHash
-}
-
-func assertTxMined(t *testing.T, r *coinharness.Harness, txid coinharness.Hash, blockHash *chainhash.Hash) {
-	block, err := r.NodeRPCClient().Internal().(*rpcclient.Client).GetBlock(blockHash)
-	if err != nil {
-		t.Fatalf("unable to get block: %v", err)
-	}
-
-	numBlockTxns := len(block.Transactions)
-	if numBlockTxns < 2 {
-		t.Fatalf("crafted transaction wasn't mined, block should have "+
-			"at least %v transactions instead has %v", 2, numBlockTxns)
-	}
-
-	txHash1 := block.Transactions[1].TxHash()
-
-	if txHash1 != txid {
-		t.Fatalf("txid's don't match, %v vs %v", txHash1, txid)
-	}
-}
 
 func TestBallance(t *testing.T) {
 	// Skip tests when running with -short
@@ -92,7 +35,12 @@ func TestSendOutputs(t *testing.T) {
 	r.Wallet.Sync(H)
 	// First, generate a small spend which will require only a single
 	// input.
-	txid := genSpend(t, r, coinharness.CoinsAmountFromFloat(5))
+	txid := coinharness.GenSpend(t, r,
+		coinharness.CoinsAmountFromFloat(5),
+		wire.DefaultPkScriptVersion,
+		dcrharness.PayToAddrScript,
+		dcrharness.TxSerializeSize,
+	)
 
 	// Generate a single block, the transaction the wallet created should
 	// be found in this block.
@@ -100,16 +48,21 @@ func TestSendOutputs(t *testing.T) {
 	if err != nil {
 		t.Fatalf("unable to generate single block: %v", err)
 	}
-	assertTxMined(t, r, txid, blockHashes[0].(*chainhash.Hash))
+	coinharness.AssertTxMined(t, r, txid, blockHashes[0])
 
 	// Next, generate a spend much greater than the block reward. This
 	// transaction should also have been mined properly.
-	txid = genSpend(t, r, coinharness.CoinsAmountFromFloat(1000))
+	txid = coinharness.GenSpend(t, r,
+		coinharness.CoinsAmountFromFloat(1000),
+		wire.DefaultPkScriptVersion,
+		dcrharness.PayToAddrScript,
+		dcrharness.TxSerializeSize,
+	)
 	blockHashes, err = r.NodeRPCClient().Generate(1)
 	if err != nil {
 		t.Fatalf("unable to generate single block: %v", err)
 	}
-	assertTxMined(t, r, txid, blockHashes[0].(*chainhash.Hash))
+	coinharness.AssertTxMined(t, r, txid, blockHashes[0])
 
 	// Generate another block to ensure the transaction is removed from the
 	// mempool.
